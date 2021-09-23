3d3216f1103a3a302f3bb58c45a81a017bf91d82
FABIO L SANTOS <fabio_9.9@hotmail.com>
2021-09-23 15:18:17 -0300

    mudança de repositório
    dia 23/09/2021

---
+++ NovoSite/js/main.js
@@ -0,0 +1,9 @@
+$('.sliderPrincipal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2500
+});
