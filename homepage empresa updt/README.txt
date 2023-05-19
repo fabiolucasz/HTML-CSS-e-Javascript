85549c70efae2232736b82ca89b6a05eadcbcdcc
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2023-05-19 20:41:26 -0300

    Add files via upload

---
+++ homepage empresa updt/README.txt
@@ -0,0 +1,3 @@
+Acesse pelo link abaixo:
+
+https://64680307e49d322b236a4a6e--astonishing-basbousa-ce1ba9.netlify.app/
---
+++ homepage empresa updt/js/main.js
@@ -0,0 +1,1 @@
+$(".sliderPrincipal").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2500});
