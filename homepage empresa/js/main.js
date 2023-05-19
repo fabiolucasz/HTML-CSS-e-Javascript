8530ca0ef71ab206a2bdd8240aed9a4dc29080e2
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2023-05-19 20:40:23 -0300

    Delete homepage empresa directory

--- homepage empresa/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.sliderPrincipal').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2500
-});
